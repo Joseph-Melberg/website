9140ed38d490dcf1391126b27952a4d6f1a155ce
Joseph-Melberg <dasbasdo@gmail.com>
2021-02-08 19:38:48 -0600

    First production JavaScript

--- Scripts/update.bash
+++ Scripts/update.bash
@@ -1,5 +1,7 @@
 cd ~/Documents/Git/website/
 echo "Pulling latest to get updated"
 git pull
+echo "Cleaning destination"
+rm -rf /var/www/html/*
 echo "Moving files"
 cp ~/Documents/Git/website/*.html /var/www/html/
